85bd75a911998fcd231ca952a530f802afeb1c23
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-23 02:54:53 +0300

    v 0.29.1

---
+++ Assets/Scripts/LineID.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineID : MonoBehaviour
+{
+    public int ID;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.)
+    }
+}
