6aa298a4872628e166c4d5c097b7f608dfad64fc
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-24 01:16:38 +0300

    0.31

--- Assets/Scripts/LineID.cs
+++ Assets/Scripts/LineID.cs
@@ -25,11 +25,9 @@ public class LineID : MonoBehaviour
         GameObject secondObject = lineManager.Objects[lineManager.Objects.Count - 2];
         this.transform.localScale = new Vector3(Vector3.Distance(firstObject.transform.position, secondObject.transform.position), 0.2f, 0.2f);
 
-
-        /*  if ((secondObject.transform.position.x - firstObject.transform.position.x > 0 && secondObject.transform.position.y - firstObject.transform.position.y > 0) || (secondObject.transform.position.x - firstObject.transform.position.x < 0 && secondObject.transform.position.y - firstObject.transform.position.y < 0))
-              this.transform.rotate = new Vector3(0, Mathf.Atan((secondObject.transform.position.x - firstObject.transform.position.x) / (secondObject.transform.position.y - firstObject.transform.position.y)), 0);
-          else
-              this.transform.rotate = new Vector3(0, Mathf.Atan((secondObject.transform.position.x - firstObject.transform.position.x) / (secondObject.transform.position.y - firstObject.transform.position.y)) + 180, 0);
-      */
+        if ((secondObject.transform.position.x - firstObject.transform.position.x > 0 && secondObject.transform.position.y - firstObject.transform.position.y > 0) || (secondObject.transform.position.x - firstObject.transform.position.x < 0 && secondObject.transform.position.y - firstObject.transform.position.y < 0))
+            this.transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.Atan((secondObject.transform.position.x - firstObject.transform.position.x) / (secondObject.transform.position.y - firstObject.transform.position.y)), 0));
+        else
+            this.transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.Atan((secondObject.transform.position.x - firstObject.transform.position.x) / (secondObject.transform.position.y - firstObject.transform.position.y)) + 180, 0));
     }
 }
