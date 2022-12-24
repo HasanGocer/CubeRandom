fd8542f3e461c392d2c79fe5175b2a870c510950
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-25 00:27:17 +0300

    v 0.35.2

--- Assets/Scripts/LineID.cs
+++ Assets/Scripts/LineID.cs
@@ -25,7 +25,7 @@ public class LineID : MonoBehaviour
         GameObject secondObject = lineManager.Objects[lineManager.Objects.Count - 2];
 
         this.transform.position = new Vector3((secondObject.transform.position.x + firstObject.transform.position.x) / 2, (secondObject.transform.position.y + firstObject.transform.position.y) / 2, (secondObject.transform.position.z + firstObject.transform.position.z) / 2);
-        this.transform.localScale = new Vector3(Vector3.Distance(firstObject.transform.position, secondObject.transform.position), 0.2f, 0.2f);
+        this.transform.localScale = new Vector3(Vector3.Distance(firstObject.transform.position, secondObject.transform.position), 0.1f, 0.1f);
 
         if ((secondObject.transform.position.x - firstObject.transform.position.x > 0 && secondObject.transform.position.y - firstObject.transform.position.y > 0) || (secondObject.transform.position.x - firstObject.transform.position.x < 0 && secondObject.transform.position.y - firstObject.transform.position.y < 0))
             this.transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.Atan((secondObject.transform.position.x - firstObject.transform.position.x) / (secondObject.transform.position.z - firstObject.transform.position.z)) * 50, 0));
