a8b818843aabfedb50c80a70953578d2a5d8d598
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-25 16:44:21 +0300

    v 0.35.3

--- Assets/Scripts/LineManager.cs
+++ Assets/Scripts/LineManager.cs
@@ -30,6 +30,7 @@ public class LineManager : MonoSingleton<LineManager>
         int limit = Objects.Count;
         for (int i = limit - 1; i >= 0; i--)
         {
+            Objects[i].GetComponent<Draw>().LineRendererCanceled();
             ObjectID objectID = Objects[i].GetComponent<ObjectID>();
             RandomSystem.Instance.ObjectGrid[objectID.lineCount, objectID.ColumnCount] = false;
             RandomSystem.Instance.ObjectPoolAdd(Objects[i], RandomSystem.Instance.ObjectList, ID);
