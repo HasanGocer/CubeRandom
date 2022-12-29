e5e857e356921839f18efeb9cbc94299a689f3f3
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-30 02:00:49 +0300

    v 0.46

--- Assets/Scripts/MaterialSystem.cs
+++ Assets/Scripts/MaterialSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class MaterialSystem : MonoSingleton<MaterialSystem>
 {
+    public List<Material> PlayerMaterials = new List<Material>();
     public List<Material> RivalMaterials = new List<Material>();
     public SkinnedMeshRenderer rivalPlayer;
 }
